7f58b0aedb3b76381c04f8981f242ff871ab75aa
Jules Boussekeyt <jules.boussekeyt@gmail.com>
2015-03-13 18:00:00 +0100

    homepage design

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Check and Notifiy
+# Check and Notifiy
+
+- Check if a (twitter|github|...) username is free
+- Test when a website goes down
+
+- Test when a retweet is RT more than x times
+- Test when a tweet have a reply, ...
+
+- Test when a website become free
+- Test when torvalds publish a new post on g+ (to be the first to comment)
+
+- When there's full moon
--- public/js/modules/public/home.js
+++ public/js/modules/public/home.js
@@ -10,33 +10,6 @@ define([
 
     layout: 'public',
 
-    data: function() {
-      return {
-        CONFIG: CONFIG,
-
-        TOOLS: [
-          {
-            name: 'firebase',
-            colors: ['sun-flower', 'orange'],
-            url: 'firebase.com'
-          },
-          {
-            name: 'requirejs',
-            colors: ['peter-river', 'belize-hole'],
-            url: 'requirejs.org'
-          },
-          {
-            name: 'vuejs',
-            colors: ['emerald', 'nephritis'],
-            url: 'vuejs.org'
-          },
-          {
-            name: 'backbone',
-            colors: ['wet-asphalt', 'midnight-blue'],
-            url: 'backbonejs.org'
-          }
-        ]
-      }
-    }
+    inherit: true
   })
 })
