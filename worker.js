f89d94d0f0772418c94f4d666d3a78d34b7a6ab0
Jules B <jules.bous@gmail.com>
2015-05-03 19:39:58 +0200

    attempt to fix CIé

--- worker.js
+++ worker.js
@@ -1,11 +1,9 @@
 require('dotenv').config({ path: 'config/env' })
 require('dotenv').load()
 
-var util = require('util')
 var starter = require('./worker/starter')
 
 starter.start(function() {
-  util.log('starting')
   starter.startQueue()
   starter.startLoop()
 })
