0a0ee43bde31f496d5b8cea7e63d43679fe95fbe
Jules B <jules.bous@gmail.com>
2015-05-07 12:25:24 +0200

    downgrade node-sass

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ done
 
 npm install -g bower
 npm install -g karma-cli
-npm install -g node-sass
+npm install -g node-sass@v2.1.1
 npm install -g autoprefixer
 npm install -g chokidar-cli
 npm update
