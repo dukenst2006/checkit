6f1943ef425fb1adb9ec8693c8b99775fc813a01
Jules B <jules.bous@gmail.com>
2015-05-13 19:14:09 +0200

    debug CI

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # set value to env variable CHECKIT_FIREBASE_URL
-sed -i '' -e 's#^.*\(FIREBASE*:*\).*$#\1"'$CHECKIT_FIREBASE_URL'"#' public/js/config.js
+sed -i '' -e 's#^.*\(FIREBASE*:*\).*$#\1"'$CHECKIT_FIREBASE_URL'"#' ./public/js/config.js
