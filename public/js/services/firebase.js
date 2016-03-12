b2f329797835647df2d2ee57a762029112636a73
Jules B <jules.bous@gmail.com>
2016-03-12 10:24:44 +0100

    remove firebase.createUser() overrides

--- public/js/services/firebase.js
+++ public/js/services/firebase.js
@@ -3,17 +3,6 @@ define([
   'fb'
 ], function(Config) {
 
-  // monkey patch for https://github.com/firebase/firebase-bower/issues/4
-  fb.login.AuthenticationManager.prototype.createUser = function(params, opt_onComplete) {
-    this.checkServerSettingsOrThrow()
-    var path = '/users'
-    var requestInfo = fb.login.RequestInfo.fromParams(params)
-    requestInfo.serverParams['_method'] = 'POST'
-    this.requestWithCredential(path, requestInfo, function(err, res) {
-      fb.core.util.callUserCallback(opt_onComplete, err, res)
-    })
-  }
-
   var FIREBASE_URL = Config.FIREBASE.replace(/(\/)?$/, '/')
 
   var firebase = new Firebase(FIREBASE_URL)
